9e4b52457737ad21576a997be6c8c52d47f38791
Michal Delura <deluramichal@gmail.com>
2020-03-27 22:53:39 +0100

    Updates

---
+++ precache-manifest.9352bb172eaf7d6dbd3ccee84cd6c042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f91410aebb36e1b55055f9f8d67c7f0",
+    "url": "/covid-19-stats/index.html"
+  },
+  {
+    "revision": "a18c9820c2532e620c81",
+    "url": "/covid-19-stats/static/js/2.bf7da1cf.chunk.js"
+  },
+  {
+    "revision": "62d9c95f255edb445364850a0508c7a0",
+    "url": "/covid-19-stats/static/js/2.bf7da1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18c3ec77afca9b8ec1e",
+    "url": "/covid-19-stats/static/js/main.25d78bf4.chunk.js"
+  },
+  {
+    "revision": "a97935a10b12fd7e2ebc",
+    "url": "/covid-19-stats/static/js/runtime-main.e812401a.js"
+  }
+]);
